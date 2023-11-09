62ce1c80234d2bef7b711aed5fd588ad9eae3cc8
pappu18 <134090952+pappu18@users.noreply.github.com>
2023-11-09 05:58:44 +0530

    Update assymmetric.py

--- assymmetric.py
+++ assymmetric.py
@@ -1,15 +1,55 @@
-import rsa
+# Python for RSA asymmetric cryptographic algorithm.
+# For demonstration, values are
+# relatively small compared to practical application
+import math
 
-# Generate RSA key pair with small key sizes (for educational purposes)
-public_key, private_key = rsa.newkeys(512)
 
-# Message to encrypt
-message = "Hello, RSA!"
+def gcd(a, h):
+	temp = 0
+	while(1):
+		temp = a % h
+		if (temp == 0):
+			return h
+		a = h
+		h = temp
 
-# Encrypt the message using the public key
-encrypted_message = rsa.encrypt(message.encode(), public_key)
-print("Encrypted message:", encrypted_message)
 
-# Decrypt the message using the private key
-decrypted_message = rsa.decrypt(encrypted_message, private_key).decode()
-print("Decrypted message:", decrypted_message)
+p = 3
+q = 7
+n = p*q
+e = 2
+phi = (p-1)*(q-1)
+
+while (e < phi):
+
+	# e must be co-prime to phi and
+	# smaller than phi.
+	if(gcd(e, phi) == 1):
+		break
+	else:
+		e = e+1
+
+# Private key (d stands for decrypt)
+# choosing d such that it satisfies
+# d*e = 1 + k * totient
+
+k = 2
+d = (1 + (k*phi))/e
+
+# Message to be encrypted
+msg = 12.0
+
+print("Message data = ", msg)
+
+# Encryption c = (msg ^ e) % n
+c = pow(msg, e)
+c = math.fmod(c, n)
+print("Encrypted data = ", c)
+
+# Decryption m = (c ^ d) % n
+m = pow(c, d)
+m = math.fmod(m, n)
+print("Original Message Sent = ", m)
+
+
+# This code is contributed by Pranay Arora.
